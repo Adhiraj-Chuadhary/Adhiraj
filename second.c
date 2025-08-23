538cbfbfe239f071422a532422b3d4f07a550ad8
Adhiraj-Chuadhary <adhirajchaudhary2007@gmail.com>
2025-08-24 02:19:21 +0530

    Day 1 sum of two numbers
    write a program to input two numbers and display their sum

---
+++ second.c
@@ -0,0 +1,12 @@
+#include<Stdio.h>
+int main(){
+    int a,b;
+    printf("enter first number");
+    scanf("%d",&a);
+    printf("enter second number");
+    scanf("%d",&b);
+    int sum = a+b;
+    printf("the sum of two numbers is;");
+    printf("%d",sum);
+    return 0;
+}
